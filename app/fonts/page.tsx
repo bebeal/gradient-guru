f8942dbc458006816b77cc8d5301699e65596170
Benjamin Noah Beal <bnoahbeal@gmail.com>
2023-11-17 03:20:12 -0800

    fonts

---
+++ app/fonts/page.tsx
@@ -0,0 +1,29 @@
+
+const FontsPage = () => {
+  const tailwindFonts = [
+    "font-mono",
+    "font-argon",
+    "font-krypton",
+    "font-neon",
+    "font-radon",
+    "font-xenon",
+  ];
+
+  return (
+    <div className="flex flex-col items-center w-full h-screen overflow-auto bg-gray-300">
+      <h1 className="text-2xl font-bold justify-self-start">Fonts</h1>
+      <div className="flex flex-wrap justify-center w-full gap-10 h-full p-2">
+        {tailwindFonts.map((font) => (
+          <div key={font} className="flex flex-col items-center justify-center p-1">
+            <div className={`p-1 w-auto h-auto ${font}`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</div>
+            <div className={`p-1 w-auto h-auto ${font} font-bold`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</div>
+            <div className={`p-1 w-auto h-auto ${font} italic`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</div>
+            <div className={`p-1 w-auto h-auto ${font} font-bold italic`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</div>
+            <p className="text-sm font-bold">{font}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+};
+export default FontsPage;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,85 +12,95 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      slate: {
-        ...colors.slate,
-        DEFAULT: colors.slate['500'],
+      colors: {
+        slate: {
+          ...colors.slate,
+          DEFAULT: colors.slate['500'],
+        },
+        zinc: {
+          ...colors.zinc,
+          DEFAULT: colors.zinc['500'],
+        },
+        stone: {
+          ...colors.stone,
+          DEFAULT: colors.stone['500'],
+        },
+        red: {
+          ...colors.red,
+          DEFAULT: colors.red['500'],
+        },
+        orange: {
+          ...colors.orange,
+          DEFAULT: colors.orange['500'],
+        },
+        amber: {
+          ...colors.amber,
+          DEFAULT: colors.amber['500'],
+        },
+        yellow: {
+          ...colors.yellow,
+          DEFAULT: colors.yellow['500'],
+        },
+        lime: {
+          ...colors.lime,
+          DEFAULT: colors.lime['500'],
+        },
+        green: {
+          ...colors.green,
+          DEFAULT: colors.green['500'],
+        },
+        emerald: {
+          ...colors.emerald,
+          DEFAULT: colors.emerald['500'],
+        },
+        teal: {
+          ...colors.teal,
+          DEFAULT: colors.teal['500'],
+        },
+        cyan: {
+          ...colors.cyan,
+          DEFAULT: colors.cyan['500'],
+        },
+        sky: {
+          ...colors.sky,
+          DEFAULT: colors.sky['500'],
+        },
+        blue: {
+          ...colors.blue,
+          DEFAULT: colors.blue['500'],
+        },
+        indigo: {
+          ...colors.indigo,
+          DEFAULT: colors.indigo['500'],
+        },
+        violet: {
+          ...colors.violet,
+          DEFAULT: colors.violet['500'],
+        },
+        purple: {
+          ...colors.purple,
+          DEFAULT: colors.purple['500'],
+        },
+        fuchia: {
+          ...colors.fuchsia,
+          DEFAULT: colors.fuchsia['500'],
+        },
+        pink: {
+          ...colors.pink,
+          DEFAULT: colors.pink['500'],
+        },
+        rose: {
+          ...colors.rose,
+          DEFAULT: colors.rose['500'],
+        },
       },
-      zinc: {
-        ...colors.zinc,
-        DEFAULT: colors.zinc['500'],
-      },
-      stone: {
-        ...colors.stone,
-        DEFAULT: colors.stone['500'],
-      },
-      red: {
-        ...colors.red,
-        DEFAULT: colors.red['500'],
-      },
-      orange: {
-        ...colors.orange,
-        DEFAULT: colors.orange['500'],
-      },
-      amber: {
-        ...colors.amber,
-        DEFAULT: colors.amber['500'],
-      },
-      yellow: {
-        ...colors.yellow,
-        DEFAULT: colors.yellow['500'],
-      },
-      lime: {
-        ...colors.lime,
-        DEFAULT: colors.lime['500'],
-      },
-      green: {
-        ...colors.green,
-        DEFAULT: colors.green['500'],
-      },
-      emerald: {
-        ...colors.emerald,
-        DEFAULT: colors.emerald['500'],
-      },
-      teal: {
-        ...colors.teal,
-        DEFAULT: colors.teal['500'],
-      },
-      cyan: {
-        ...colors.cyan,
-        DEFAULT: colors.cyan['500'],
-      },
-      sky: {
-        ...colors.sky,
-        DEFAULT: colors.sky['500'],
-      },
-      blue: {
-        ...colors.blue,
-        DEFAULT: colors.blue['500'],
-      },
-      indigo: {
-        ...colors.indigo,
-        DEFAULT: colors.indigo['500'],
-      },
-      violet: {
-        ...colors.violet,
-        DEFAULT: colors.violet['500'],
-      },
-      purple: {
-        ...colors.purple,
-        DEFAULT: colors.purple['500'],
-      },
-      fuchia: {
-        ...colors.fuchsia,
-        DEFAULT: colors.fuchsia['500'],
-      },
-      pink: {
-        ...colors.pink,
-        DEFAULT: colors.pink['500'],
-      },
-      rose: {
-        ...colors.rose,
-        DEFAULT: colors.rose['500'],
+      fontFamily: {
+        mono: ["Berkeley Mono", "monospace"],
+        argon: ["Argon", "Berkeley Mono", "monospace"],
+        krypton: ["Krypton", "Berkeley Mono", "monospace"],
+        neon: ["Neon", "Berkeley Mono", "monospace"],
+        radon: ["Radon", "Berkeley Mono", "monospace"],
+        xenon: ["Xenon", "Berkeley Mono", "monospace"],
       },
     },
   },
