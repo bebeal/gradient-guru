cf1bf3786cd6d47268a4074271630cb62c07c352
Benjamin Noah Beal <bnoahbeal@gmail.com>
2024-01-25 02:01:46 -0800

    idk

--- components/Flow/Extensions/DropWrapper.tsx
+++ components/Flow/Extensions/DropWrapper.tsx
@@ -22,41 +22,59 @@ export const DropWrapper = (props: DropWrapperProps) => {
   const { children } = props;
   const editor = useEditor();
 
-
-  const dropNode = useCallback((point: any, parsedData: any, defaultProps: any) => {
-    const node = {
-      ...parsedData,
-      id: createShapeId(),
-      x: point.x - (defaultProps?.w / 2 || 0),
-      y: point.y - (defaultProps?.h / 2 || 0),
-    } as TLShape;
+  const dropNode = useCallback((node: TLShape) => {
     editor.createShape(node);
     setTimeout(() => {
       zoomToFitNewNode(editor);
     }, 0);
   }, [editor]);
 
-  const handleDrop = useCallback((event: any) => {
+  const handleDrop = useCallback((event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
 
     const point = editor.screenToPage({ x: event.clientX, y: event.clientY });
     const nodeData = event.dataTransfer.getData("application/tldraw");
     const parsedData = JSON.parse(nodeData);
     const type = parsedData?.type;
-    // is either a single node indicated by type
+
     if (type) {
       if (editor.shapeUtils[type] === undefined) return;
-      const defaultProps = editor.getShapeUtil(parsedData?.type).getDefaultProps();
-      dropNode(point, parsedData, defaultProps);
+      const defaultProps = editor.getShapeUtil(type).getDefaultProps();
+      const node: TLShape = {
+        ...parsedData,
+        id: createShapeId(),
+        x: point.x - (defaultProps?.w / 2 || 0),
+        y: point.y - (defaultProps?.h / 2 || 0),
+      };
+      dropNode(node);
     } else {
-      // or a group of nodes indicated by nodesToAdd
       const nodesToAdd = parsedData?.nodesToAdd;
+
+      // Calculate collective bounds
+      let minX = Infinity, minY = Infinity, maxX = -Infinity, maxY = -Infinity;
       nodesToAdd.forEach((node: any) => {
-        dropNode(point, node, node.props);
+        const bounds = editor.getShapePageBounds(node)!;
+        minX = Math.min(minX, bounds.minX);
+        minY = Math.min(minY, bounds.minY);
+        maxX = Math.max(maxX, bounds.maxX);
+        maxY = Math.max(maxY, bounds.maxY);
+      });
+
+      const center = { x: (minX + maxX) / 2, y: (minY + maxY) / 2 };
+      const offset = { x: point.x - center.x, y: point.y - center.y };
+
+      nodesToAdd.forEach((node: any) => {
+        const adjustedNode: TLShape = {
+          ...node,
+          id: createShapeId(),
+          x: node.x + offset.x,
+          y: node.y + offset.y,
+        };
+        dropNode(adjustedNode);
       });
     }
 
-  }, [dropNode, editor] );
+  }, [dropNode, editor]);
 
-  return ( <DropWrapperContainer onDrop={handleDrop}>{children}</DropWrapperContainer> );
+  return <DropWrapperContainer onDrop={handleDrop}>{children}</DropWrapperContainer>;
 };
