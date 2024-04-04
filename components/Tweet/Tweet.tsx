c859d525beb390acdc651e11bf1569f6dd29aad3
Benjamin Noah Beal <bnoahbeal@gmail.com>
2024-04-04 16:24:20 -0700

    Remove unnecessary tweet styles

--- components/Tweet/Tweet.tsx
+++ components/Tweet/Tweet.tsx
@@ -2,8 +2,6 @@
 
 import { EmbeddedTweet, TweetNotFound, TweetSkeleton, useTweet, type TweetProps } from 'react-tweet';
 
-import './tweet.css';
-
 export const Tweet = (props: TweetProps) => {
   const { id, components, ...rest } = props;
   const { data, error, isLoading } = useTweet(id, `/api/tweet/${id}`);
@@ -16,7 +14,7 @@ export const Tweet = (props: TweetProps) => {
     return <TweetSkeleton />;
   }
   return (
-    <div className="tweet my-6">
+    <div className="my-6">
       <div className={`flex justify-center`}>
         <EmbeddedTweet tweet={data} components={components} {...rest} />
       </div>
