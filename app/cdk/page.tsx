2dee85b523f0f6db1c37aec5914cd4501f4674ae
Benjamin Noah Beal <bnoahbeal@gmail.com>
2024-04-07 02:23:12 -0700

    Add CDK

---
+++ app/cdk/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { ReactElement } from "react";
+import CDKREADME from '@/cdk/README.md';
+
+const CDKREADMEPage = (): ReactElement => {
+  return (
+    <CDKREADME />
+  )
+};
+
+export default CDKREADMEPage;
