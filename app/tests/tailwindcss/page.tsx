97353791c231ccd1ce69a03edb447c420bdb9b4b
Benjamin Noah Beal <bnoahbeal@gmail.com>
2024-07-16 02:48:55 -0700

    tailwindcss code

--- app/tests/tailwindcss/page.tsx
+++ app/tests/tailwindcss/page.tsx
@@ -197,37 +197,65 @@ const ColorPaletteReference = ({ colors = [] }: { colors?: string[] }) => {
 };
 
 const GridDemo = () => {
+  const gridCode = `<div class="bg-grid-white/[0.2]">grid</div>`;
   return (
     <div className="h-[450px] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="flex flex-row gap-10">
+          <div className="flex flex-col w-auto h-auto items-center justify-center overflow-hidden">
+            <Terminal language={"jsx"}>{gridCode}</Terminal>
+          </div>
+          <DivArrow />
       <p className="text-4xl sm:text-2xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">bg-grid</p>
+      </div>
     </div>
   );
 };
 
 const GridSmallDemo = () => {
+  const smallGridCode = `<div class="bg-grid-sm-white/[0.2]">small grid</div>`;
   return (
     <div className="h-[450px] w-full dark:bg-black bg-white  dark:bg-grid-sm-white/[0.2] bg-grid-sm-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="flex flex-row gap-10">
+          <div className="flex flex-col w-auto h-auto items-center justify-center overflow-hidden">
+            <Terminal language={"jsx"}>{smallGridCode}</Terminal>
+          </div>
+          <DivArrow />
       <p className="text-4xl sm:text-2xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">bg-grid-sm</p>
+      </div>
     </div>
   );
 };
 
 const DotDemo = () => {
+  const dotCode = `<div class="bg-dot-white/[0.2]">dot</div>`;
   return (
     <div className="h-[450px] w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="flex flex-row gap-10">
+          <div className="flex flex-col w-auto h-auto items-center justify-center overflow-hidden">
+            <Terminal language={"jsx"}>{dotCode}</Terminal>
+          </div>
+          <DivArrow />
       <p className="text-4xl sm:text-2xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">bg-dot</p>
+      </div>
     </div>
   );
 };
 
 const GlassDemo = () => {
+  const glassCode = `<div class="glass">glass</div>`;
   return (
     <div className="bg-mountain bg-contain bg-no-repeat bg-center h-[450px] w-full flex justify-center items-center">
-      <div className="glass h-[15rem] w-[25rem] text-[#b5b5b5] font-bold rounded-md text-2xl flex items-center justify-center">
+      <div className="flex flex-row gap-10">
+          <div className="flex flex-col w-auto h-auto items-center justify-center overflow-hidden">
+            <Terminal language={"jsx"}>{glassCode}</Terminal>
+          </div>
+          <DivArrow />
+      <div className="glass h-[15rem] w-[20rem] text-[#b5b5b5] font-bold rounded-md text-2xl flex items-center justify-center">
         glass
+      </div>
       </div>
     </div>
   );
