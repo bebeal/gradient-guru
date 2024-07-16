6b7a1c866cfa7d556a21aa2435ae754da9c5b142
Benjamin Noah Beal <bnoahbeal@gmail.com>
2024-07-16 01:38:53 -0700

    link fonts

--- app/tests/assets/fonts/page.tsx
+++ app/tests/assets/fonts/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Highlighter } from "@/components/Highlighter/Highlighter";
 import { cn } from "@/utils";
 
 const FontDemo = ({ fontClassName, className }: { fontClassName: string; className?: string }) => (
@@ -22,12 +23,36 @@ const FontDemos = ({ fontClassName }: { fontClassName: string }) => (
 
 const FontsPage = () => {
   const tailwindFonts = [
-    "font-mono",
-    "font-argon",
-    "font-krypton",
-    "font-neon",
-    "font-radon",
-    "font-xenon",
+    {
+      fontClassName: "font-mono",
+      name: 'Berkeley Mono',
+      url: 'https://berkeleygraphics.com/typefaces/berkeley-mono/',
+    },
+    {
+      fontClassName: "font-argon",
+      name: 'Github Monaspace - Argon (Humanist sans)',
+      url: 'https://monaspace.githubnext.com/#learn-more'
+    },
+    {
+      fontClassName: "font-krypton",
+      name: 'Github Monaspace - Krypton (Mechanical sans)',
+      url: 'https://monaspace.githubnext.com/#learn-more'
+    },
+    {
+      fontClassName: "font-neon",
+      name: 'Github Monaspace - Neon (Neo-grotesque sans)',
+      url: 'https://monaspace.githubnext.com/#learn-more'
+    },
+    {
+      fontClassName: "font-radon",
+      name: 'Github Monaspace - Radon (Handwriting)',
+      url: 'https://monaspace.githubnext.com/#learn-more'
+    },
+    {
+      fontClassName: "font-xenon",
+      name: 'Github Monaspace - Xenon (Slab serif)',
+      url: 'https://monaspace.githubnext.com/#learn-more'
+    }
   ];
 
   return (
@@ -35,9 +60,13 @@ const FontsPage = () => {
       <h1 className="text-2xl font-bold justify-self-start">Fonts Showcase</h1>
       <div className="flex flex-wrap justify-center w-full gap-10 p-2">
         {tailwindFonts.map((font) => (
-          <div key={font} className="flex flex-col rounded items-center justify-center w-full h-auto overflow-auto bg-primary text-primary gap-2 border border-primary p-2">
-            <code className={`text-sm font-bold`}>{font}</code>
-            <FontDemos fontClassName={font} />
+          <div key={`font-${font.fontClassName}`} className="flex flex-col rounded items-center justify-center w-full h-auto overflow-auto bg-primary text-primary gap-1 border border-primary p-2">
+            <div className="flex flex-row gap-4 h-auto items-center">
+              <Highlighter language={"bash"} className="font-bold">{font.fontClassName}</Highlighter>
+              -
+              <a key={`link-${font.url}`} href={font.url} target="_blank" rel="noopener noreferrer" className="font-bold" >{font.name}</a>
+            </div>
+            <FontDemos {...font} />
           </div>
         ))}
       </div>
--- components/Links/LinkList.tsx
+++ components/Links/LinkList.tsx
@@ -9,13 +9,7 @@ export const LinkList = (props: LinkListProps) => {
   return (
     <div>
       {links.map((link, index) => (
-        <a
-          key={link}
-          href={`${prefix}${link}`}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-lg font-bold block my-2"
-        >
+        <a key={link} href={`${prefix}${link}`} target="_blank" rel="noopener noreferrer" className="text-lg font-bold block my-2" >
           {textMap && textMap[link] ? textMap[link] : link}
         </a>
       ))}
